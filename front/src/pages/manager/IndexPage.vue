9732d42b6285bc5e8001a99861ea1a68e990d79b
Djangoner <djangoner6@gmail.com>
2023-09-12 15:27:51 +0300

    upd: replaced index page to aside menu

--- front/src/pages/manager/IndexPage.vue
+++ front/src/pages/manager/IndexPage.vue
@@ -1,9 +1,15 @@
 <template>
-  <q-page padding>
-    Manager index
+  <q-page
+    class="flex flex-center"
+    padding
+  >
+    <div>
+      <main-aside-menu />
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import MainAsideMenu from '../../components/common/MainAsideMenu.vue'
 
 </script>
