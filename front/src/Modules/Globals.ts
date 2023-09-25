0403548c3eca3d27d96466067c6ed1b0235868ac
Djangoner <djangoner6@gmail.com>
2023-09-25 18:54:00 +0300

    fix: url validation

--- front/src/Modules/Globals.ts
+++ front/src/Modules/Globals.ts
@@ -3,22 +3,17 @@ export const ruleValidEmail = (val: string) => {
   const emailPattern = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/
   return emailPattern.test(val) || "Введите валидный email адрес "
 }
+
+function isValidURL(urlString: string) {
+  try {
+    return Boolean(new URL(urlString))
+  } catch (e) {
+    return false
+  }
+}
+
 export const ruleValidURL = (val: string) => {
-  const pattern = new RegExp(
-    "^(https?:\\/\\/)?" + // protocol
-      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-      "(\\#[-a-z\\d_]*)?$",
-    "i"
-  ) // fragment locator
-
-  // Remove django template items
-  const clearRegex = new RegExp("{[{%][^}]*[%}]}")
-  const valClean = val.replace(clearRegex, "")
-
-  return pattern.test(valClean) || "Введите валидный URL "
+  return isValidURL(val) || "Введите валидный URL "
 }
 export const ruleValidJSON = (val: string) => {
   return isJsonString(val) || "Введите валидный JSON"
